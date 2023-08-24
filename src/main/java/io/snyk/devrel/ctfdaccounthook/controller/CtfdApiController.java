e228bc65e0b268674b9ddf347697350cea64a62d
Micah Silverman <micah@afitnerd.com>
2023-08-23 21:32:14 -0400

    WIP - bulk update and email users by affiliation. Needs to be threaded and needs backoff logic.

--- src/main/java/io/snyk/devrel/ctfdaccounthook/controller/CtfdApiController.java
+++ src/main/java/io/snyk/devrel/ctfdaccounthook/controller/CtfdApiController.java
@@ -4,12 +4,18 @@ import io.snyk.devrel.ctfdaccounthook.Exception.CtfdApiException;
 import io.snyk.devrel.ctfdaccounthook.model.CtfdApiErrorResponse;
 import io.snyk.devrel.ctfdaccounthook.model.CtfdCreateUserRequest;
 import io.snyk.devrel.ctfdaccounthook.model.CtfdResponse;
+import io.snyk.devrel.ctfdaccounthook.model.CtfdUpdateAndEmailResponse;
+import io.snyk.devrel.ctfdaccounthook.model.CtfdUser;
+import io.snyk.devrel.ctfdaccounthook.model.CtfdUserPaginatedResponse;
 import io.snyk.devrel.ctfdaccounthook.service.AliasService;
 import io.snyk.devrel.ctfdaccounthook.service.CtfdApiService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +28,8 @@ public class CtfdApiController {
     private Integer aliasRetries;
     private final CtfdApiService ctfdApiService;
     private final AliasService aliasService;
+
+    private static final Logger log = LoggerFactory.getLogger(CtfdApiController.class);
 
     public CtfdApiController(CtfdApiService ctfdApiService, AliasService aliasService) {
         this.ctfdApiService = ctfdApiService;
@@ -64,5 +72,30 @@ public class CtfdApiController {
             res.setStatus(HttpStatus.BAD_REQUEST.value());
             return e.getCtfdApiError();
         }
+    }
+
+    @PostMapping("/api/v1/update-and-email/{affiliation}")
+    public CtfdResponse updateAndEmailUsers(@PathVariable String affiliation, HttpServletResponse res) {
+        Integer page = 1;
+        int processed = 0;
+        do {
+            try {
+                CtfdUserPaginatedResponse ctfdUserResponse =
+                    ctfdApiService.getUsersByAffiliation(affiliation, page);
+                for (CtfdUser ctfdUser : ctfdUserResponse.getData()) {
+                    log.debug("Processing user id: {}, name: {}", ctfdUser.getId(), ctfdUser.getName());
+                    ctfdUser = ctfdApiService.updatePassword(ctfdUser);
+                    log.debug("Password updated for user id: {}", ctfdUser.getId());
+                    ctfdApiService.emailUser(ctfdUser);
+                    log.debug("Email sent for user id: {}", ctfdUser.getId());
+                }
+                page = ctfdUserResponse.getMeta().getPagination().getNext();
+                processed += ctfdUserResponse.getData().length;
+            } catch (CtfdApiException e) {
+                res.setStatus(HttpStatus.BAD_REQUEST.value());
+                return e.getCtfdApiError();
+            }
+        } while (page != null);
+        return new CtfdUpdateAndEmailResponse(processed);
     }
 }
--- src/main/java/io/snyk/devrel/ctfdaccounthook/service/CtfdApiServiceImpl.java
+++ src/main/java/io/snyk/devrel/ctfdaccounthook/service/CtfdApiServiceImpl.java
@@ -7,7 +7,10 @@ import io.snyk.devrel.ctfdaccounthook.model.CtfdCreateUserResponse;
 import io.snyk.devrel.ctfdaccounthook.model.CtfdUser;
 import io.snyk.devrel.ctfdaccounthook.model.CtfdUserPaginatedResponse;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -27,10 +30,21 @@ public class CtfdApiServiceImpl implements CtfdApiService {
     @Value("#{ @environment['ctfd.api.affiliation'] }")
     private String affiliation;
 
+    @Value("#{ @environment['ctfd.api.email-template'] }")
+    private String emailTemplate;
+
+    @Value("#{ @environment['ctfd.info.name'] }")
+    private String ctfdName;
+
+    @Value("#{ @environment['ctfd.info.url'] }")
+    private String ctfdUrl;
+
     private final WebClient.Builder webClientBuilder;
     private WebClient webClient;
 
     public static final String API_URI = "/api/v1";
+
+    private static final Logger log = LoggerFactory.getLogger(CtfdApiServiceImpl.class);
 
     public CtfdApiServiceImpl(WebClient.Builder webClientBuilder) {
         this.webClientBuilder = webClientBuilder;
@@ -79,6 +93,57 @@ public class CtfdApiServiceImpl implements CtfdApiService {
         }
         CtfdApiErrorResponse error = new CtfdApiErrorResponse();
         error.getErrors().setMessage(String.format("Unable to get page %d for affiliation: %s", page, affiliation));
+        throw new CtfdApiException(error);
+    }
+
+    @Override
+    public CtfdCreateUserResponse updateUser(CtfdUser ctfdUser) {
+        if (ctfdUser == null || ctfdUser.getId() == null) {
+            log.debug("passed in CtfdUser is null or its id is null.");
+            return null;
+        }
+        ClientResponse res = this.webClient.patch().uri(API_URI + "/users/" + ctfdUser.getId())
+            .body(BodyInserters.fromValue(ctfdUser))
+            .exchange()
+            .block();
+        if (res.statusCode().is2xxSuccessful()) {
+            return res.bodyToMono(CtfdCreateUserResponse.class).block();
+        }
+        CtfdApiErrorResponse error = res.bodyToMono(CtfdApiErrorResponse.class).block();
+        throw new CtfdApiException(error);
+    }
+
+    @Override
+    public CtfdUser updatePassword(CtfdUser ctfdUser) {
+        String newPassword = UUID.randomUUID().toString();
+        ctfdUser.setPassword(newPassword);
+        updateUser(ctfdUser);
+        return ctfdUser;
+    }
+
+    @Override
+    public CtfdCreateUserResponse emailUser(CtfdUser ctfdUser) {
+        String emailText = emailTemplate
+            .replace("{ctf-name}", ctfdName)
+            .replace("{url}", ctfdUrl)
+            .replace("{name}", ctfdUser.getName())
+            .replace("{password}", ctfdUser.getPassword());
+        String uri = API_URI + "/users/" + ctfdUser.getId() + "/email";
+        ClientResponse res = this.webClient.post().uri(uri)
+            .bodyValue(emailText)
+            .exchange()
+            .block();
+        if (res.statusCode().is2xxSuccessful()) {
+            return res.bodyToMono(CtfdCreateUserResponse.class).block();
+        } else if (res.statusCode().isSameCodeAs(HttpStatus.TOO_MANY_REQUESTS)) {
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return emailUser(ctfdUser);
+        }
+        CtfdApiErrorResponse error = res.bodyToMono(CtfdApiErrorResponse.class).block();
         throw new CtfdApiException(error);
     }
 }

28d7f35f4d7d39bfe3485e3d01e2ca4b8c9d5485
Micah Silverman <micah@afitnerd.com>
2023-09-06 17:32:26 -0400

    updated security config for csrf

--- src/main/java/dev/dogeared/ctfdaccounthook/config/SecurityConfig.java
+++ src/main/java/dev/dogeared/ctfdaccounthook/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.csrf(config -> config.ignoringRequestMatchers("/api/v1/**"))
             .authorizeHttpRequests(req -> req.anyRequest().authenticated())
             .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
             .addFilterBefore(
